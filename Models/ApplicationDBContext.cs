17b8bb51b0387b6b5cbb7b7f6d87a798a58945ca
Nam VH <vo.hai.nam@gosei.com.vn>
2019-07-21 02:08:05 +0700

    init db

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+               : base(options)
+        { }
+    }
+}
