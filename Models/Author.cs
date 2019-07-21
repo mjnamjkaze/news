bfef7d9ec91b3044d657f99f0c534a4e448503bc
Nam VH <vo.hai.nam@gosei.com.vn>
2019-07-21 11:22:21 +0700

    update source

---
+++ Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class Author  : IdentityUser
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
